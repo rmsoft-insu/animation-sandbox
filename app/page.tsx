100110d096aef943fa61023064f1219d7de701a4
insu <hinsu@rmsoft.kr>
2024-06-11 19:24:01 +0900

    bento grid & grid 예제 데이터 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+import { Grid } from "@/components/grid";
 import { Hero } from "@/components/hero";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="bg-black-100 relative mx-auto flex flex-col items-center justify-center overflow-hidden px-5 sm:px-10">
       <div className="w-full max-w-7xl">
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
