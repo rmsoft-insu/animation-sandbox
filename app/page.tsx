44824ff63568af0459f68b3f9f8e5a815849861d
insu <hinsu@rmsoft.kr>
2024-06-12 14:46:06 +0900

    Navbar 수정

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,13 @@ import { Grid } from "@/components/grid";
 import { Hero } from "@/components/hero";
 import { RecentProjects } from "@/components/recent-projects";
 import { FloatingNav } from "@/components/ui/floating-navbar";
-import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
       <div className="w-full max-w-7xl">
-        <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
