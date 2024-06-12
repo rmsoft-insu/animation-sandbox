5c5a8116522f1f1b3e98627490d7644e699d0162
insu <hinsu@rmsoft.kr>
2024-06-12 14:05:17 +0900

    recent project 파트 시작

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import { Grid } from "@/components/grid";
 import { Hero } from "@/components/hero";
+import { RecentProjects } from "@/components/recent-projects";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="bg-black-100 relative mx-auto flex flex-col items-center justify-center overflow-hidden px-5 sm:px-10">
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
       <div className="w-full max-w-7xl">
         <FloatingNav
           navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
         />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
